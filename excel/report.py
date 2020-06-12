0f41f4d6f23c7bd3aaa1faa863b96155559d3b54
root <ron.gorlik@gmail.com>
2020-06-12 13:59:40 +0300

    test

---
+++ excel/report.py
@@ -0,0 +1,29 @@
+from datetime import date
+from datetime import datetime
+from xlwt import Workbook 
+
+class Report:
+
+	def __init__(self):
+		self.row=1
+		self.wb = Workbook()
+		self.current_date = date.today().strftime("%d_%m_%Y")
+		self.report  = self.wb.add_sheet(self.current_date) 
+
+		self.report.write(0,0,"time")
+		self.report.write(0,1,"address")
+
+		self.save()
+	
+	def add_row(self,src,dest):
+		current_time = datetime.now().strftime("%H:%M:%S")
+		self.report.write(self.row,0, current_time )
+		self.report.write(self.row,1, src )
+		self.report.write(self.row,2,dest)
+		self.row = self.row+1
+
+		self.save()
+
+	def save(self):
+                self.wb.save("Reports/"+self.current_date+".xls")
+
---
+++ excel/test.py
@@ -0,0 +1,6 @@
+from report import Report
+
+r = Report()
+r.add_row("192.168.1.1")
+r.add_row("walla.co.il")
+
